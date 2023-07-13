b6dcf7cef0ad01098e1e816c268af2cb0a232b29
QingZhuMr <821636751@qq.com>
2023-07-13 16:16:36 +0800

    更新本地文件

---
+++ public/static/js/common.js
@@ -0,0 +1,13 @@
+function back(){
+    window.history.back(-1);
+}
+function meet() {
+    window.location.href = "/home/index/meet";
+}
+function user(){
+    window.location.href = "/home/index/user";
+}
+function userInfo(){
+    window.location.href = "/home/user/userinfo";
+}
+
