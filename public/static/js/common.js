17cd65a510afcd81e690c29af77aa3d94c4db491
QingZhuMr <821636751@qq.com>
2023-07-14 16:26:53 +0800

    弹出层方式代替新页面

--- public/static/js/common.js
+++ public/static/js/common.js
@@ -1,13 +1,3 @@
 function back(){
     window.history.back(-1);
 }
-function meet() {
-    window.location.href = "/home/index/meet";
-}
-function user(){
-    window.location.href = "/home/index/user";
-}
-function userInfo(){
-    window.location.href = "/home/user/userinfo";
-}
-
